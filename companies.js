d95afdf5304137ba667b95595b16cc1631200504
Johan Beronius <johan.beronius@athega.se>
2016-11-17 20:31:51 +0100

    Updated companies bubbles d3/svg animation.

--- companies.js
+++ companies.js
@@ -2,88 +2,126 @@ $(function() {
     'use strict';
 
     const diameter = 640,
-        transitionDuration = 6000;
+        initialTransitionDuration = 12000,
+        initialTransitionDelay = 2000,
+        updateTransitionDuration = 800,
+        updateTransitionDelay = 200;
 
-    const color = d3.scale.category20c();
-    //const color = d3.scale.linear().domain([1,40])
-    //    .interpolate(d3.interpolateHcl)
-    //    .range([d3.rgb("#FF2000"), d3.rgb('#BF6000')]);
+    const color = d3.scaleOrdinal(d3.schemeCategory10);
 
     const svg = d3.select('#companies').append('svg')
         .attr('viewBox', '0 0 ' + diameter + ' ' + diameter)
         .attr('width', diameter)
         .attr('height', diameter);
 
-    const bubble = d3.layout.pack()
-        .sort(null)
-        .size([diameter, diameter])
-        .padding(1.5);
+    const pack = d3.pack()
+        .size([diameter, diameter]);
 
-    let arrivals = {};
+    let data = {
+        children: [],
+        companies: {},
+    };
 
     window.companiesBubbles = {
         update: function(name, arrived) {
-            // Reveived updated data event, but page may not be visible.
-            arrivals[name] = arrived;
-            updateBubbles();
+           if (data.companies[name]) {
+               data.companies[name].size = arrived;
+           } else {
+               data.children.push(data.companies[name] = {name: name, size: Math.random() * 12 + 1}); // TODO: Change random size to: arrived
+           }
         },
         pause: function() {
-            // Companies page not visible.
+            resetBubbles();
         },
         play: function() {
-            // Companies page is shown, run animations.
-        }
+            updateBubbles();
+        },
     };
 
-    function d3data(arrivals) {
-        return {
-            children: Object.keys(arrivals).map((company) => {
-                return {key: company, value: arrivals[company]};
-            })
-        };
-    }
-
     function updateBubbles() {
-        const data = d3data(arrivals);
+        var root = d3.hierarchy(data)
+            .sum((d) => d.size);
 
-        if (!data || !data.children.length) {
-            console.log('No data, bailing out');
-            return;
-        }
         var node = svg.selectAll('.node')
-            .data(bubble.nodes(data).filter((d) => !d.children));
+            .data(pack(root).leaves());
 
-        // capture the enter selection
-        var nodeEnter = node.enter()
+        // New nodes
+        var g1 = node.enter()
             .append('g')
             .attr('class', 'node')
-            .attr('transform', (d) => 'translate(' + d.x + ',' + d.y + ')');
+            .attr('transform', (d) => 'translate(' + diameter/2 + ',' + diameter/2 + ') scale(0)');
 
-        // re-use enter selection for circles
-        nodeEnter
-            .append('circle')
-            .attr('r',  (d)  => d.r )
-            .style('fill', (d) => color(d.key));
+        var i = 0;
+        g1.transition()
+            .duration(initialTransitionDuration)
+            .delay((d) => i++ * initialTransitionDelay)
+            .ease(d3.easeBounce)
+            .attr('transform', (d) => 'translate(0,0) scale(1)');
 
-        // re-use enter selection for titles
-        nodeEnter.append('text')
+        var g2 = g1.append('g')
+            .attr('transform', (d) => 'translate(' + d.x + ',' + d.y + ')')
+
+        g2.append('image')
+            .attr('xlink:href', 'images/bubble-background.png')
+            .attr('x', (d) => -d.r)
+            .attr('y', (d) => -d.r)
+            .attr('width', (d) => d.r*2)
+            .attr('height', (d) => d.r*2);
+
+        g2.append('circle')
+            .attr('r',  (d)  => d.r)
+            .style('opacity', 0.3)
+            .style('fill', (d) => color(d.data.name));
+
+        g2.append('text')
             .attr('dy', '.3em')
             .style('text-anchor', 'middle')
-            //.style('font-size', function(d) { return Math.min(2 * d.r, (2 * d.r - 8) / this.getComputedTextLength() * 24) + "px"; })
-            .text((d) => d.key);
+            .attr('lengthAdjust', 'spacingAndGlyphs')
+            .attr('textLength', (d) => d.r * 1.6)
+            .style('font-size', (d) => (d.r*0.9 - d.r*0.8 * Math.min(24, d.data.name.length)/24 ) + 'px')
+            .text((d) => d.data.name);
 
+        // Updates
+        var i = 0;
+        node.select('g')
+            .transition()
+            .duration(updateTransitionDuration)
+            .delay((d) => i++ * updateTransitionDelay)
+            .ease(d3.easeBounce)
+            .attr('transform', (d) => 'translate(' + d.x + ',' + d.y + ') scale(1)');
+
+        var i = 0;
+        node.select('image')
+            .transition()
+            .duration(updateTransitionDuration)
+            .delay((d) => i++ * updateTransitionDelay)
+            .ease(d3.easeBounce)
+            .attr('x', (d) => -d.r)
+            .attr('y', (d) => -d.r)
+            .attr('width', (d) => d.r*2)
+            .attr('height', (d) => d.r*2);
+
+        var i = 0;
         node.select('circle')
+            .transition()
+            .duration(updateTransitionDuration)
+            .delay((d) => i++ * updateTransitionDelay)
+            .ease(d3.easeBounce)
+            .attr('r', (d) => d.r);
 
-            .transition().duration(transitionDuration)
-            .attr('r', (d) => d.r)
-            .style('fill', (d) => color(d.key));
-
-        node.transition().duration(transitionDuration)
-            .attr('transform', (d) => 'translate(' + d.x + ',' + d.y + ')');
+        var i = 0;
+        node.select('text')
+            .transition()
+            .duration(updateTransitionDuration)
+            .delay((d) => i++ * updateTransitionDelay)
+            .attr('textLength', (d) => d.r * 1.6)
+            .style('font-size', (d) => (d.r*0.9 - d.r*0.8 * Math.min(24, d.data.name.length)/24 ) + 'px');
 
         node.exit().remove();
+    }
 
-        //d3.select(self.frameElement).style('height', diameter + 'px');
+    function resetBubbles() {
+        svg.selectAll('.node').remove();
     }
 
 });
