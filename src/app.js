b8b63c0a6808a69994c814151ec8b0286314ba7f
Johan Beronius <johan.beronius@athega.se>
2016-11-25 16:25:33 +0100

    Moved updates for items pages to a separate file.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 includeScript("main.js");
 includeScript("logo.js");
 includeScript("arrival.js");
+includeScript("items.js");
 includeScript("companies.js");
--- src/main.js
+++ src/main.js
@@ -98,89 +98,15 @@ $(function() {
     }
 
     function updateItemPage(name, count) {
-        var $page = $pages.filter('#' + name),
-            $list = $page.find('ul');
-
-        $page.data('count', count);
-        $page.find('h3').text(count);
-        addItems($list, count);
-
+        var $page = $pages.filter('#' + name);
+        $page.triggerHandler('update', count);
         subscription($page);
     }
 
     function updateAttendancePage(update) {
-        var name = 'attendance',
-            $page = $pages.filter('#' + name),
-            data = $page.data(),
-            $list = $page.find('ul');
-
-        $page.data(update);
-        $page.data('count', data.arrived);
-        $page.find('.arrived > h3').text(data.arrived);
-        $page.find('.present > h3').text(data.arrived - data.departed);
-        $page.find('.departed > h3').text(data.departed);
-        addItems($list, data.arrived).eq(-data.departed).addClass('departed');
-
+        var $page = $pages.filter('#attendance');
+        $page.triggerHandler('update', update);
         subscription($page);
-    }
-
-    function addItems($list, count) {
-
-        // Sine frequencies
-        var rules = document.styleSheets[0].cssRules,
-            durations = [
-                (4 + 3 * Math.random()), // wave-scale
-                1,                       // pop-in
-                (4 + 3 * Math.random()), // wave-x-left
-                (4 + 3 * Math.random()), // wave-x-margin
-                (3 + 2 * Math.random()), // wave-y-top
-                (3 + 2 * Math.random())  // wave-y-margin
-            ];
-
-        for (var i = 0; i < rules.length; i++) {
-            if (rules[i].selectorText == 'ul > li') {
-                rules[i].style['animation-duration'] =
-                    durations[0] + 's, ' + // wave-scale
-                    durations[1] + 's, ' + // pop-in
-                    durations[2] + 's, ' + // wave-x-left
-                    durations[3] + 's, ' + // wave-x-margin
-                    durations[4] + 's, ' + // wave-y-top
-                    durations[5] + 's';    // wave-y-margin
-
-                break;
-            }
-        }
-
-        // Sine phases
-        var offsets = [
-               durations[1],                        // wave-scale, start after pop-in is done.
-               0,                                   // pop-in
-               -durations[2] * (1 + Math.random()), // wave-x-left, start waves anywhere in the cycle, with negative offset to make sure they have started when pop-in starts.
-               -durations[3] * (1 + Math.random()), // wave-x-margin
-               -durations[4] * (1 + Math.random()), // wave-y-top
-               -durations[5] * (1 + Math.random())  // wave-y-margin
-            ],
-            expand = 0.5 + -0.8 * (Math.sin((Math.PI / 2) * Math.min(maxItems, count) / maxItems)),
-            delays = [
-                undefined,                 // wave-scale
-                0.7 + 0.5 * Math.random(), // pop-in
-                0.3 + expand + 0.2 * Math.random(), // wave-x-left
-                0.4 + expand + 0.2 * Math.random(), // wave-x-margin
-                0.3 + expand + 0.3 * Math.random(), // wave-y-top
-                0.4 + expand + 0.3 * Math.random()  // wave-y-margin
-            ];
-
-        return $list.empty()
-            .append('<li>'.repeat(Math.min(maxItems, count)))
-            .children()
-            .css('animation-delay', function(i) {
-                return (offsets[0] + i * delays[1]) + 's,' + // wave-scale, relative to pop-in delay.
-                       (offsets[1] + i * delays[1]) + 's,' + // pop-in
-                       (offsets[2] + i * delays[2]) + 's,' + // wave-x-left
-                       (offsets[3] + i * delays[3]) + 's,' + // wave-x-margin
-                       (offsets[4] + i * delays[4]) + 's,' + // wave-y-top
-                       (offsets[5] + i * delays[5]) + 's';   // wave-y-margin
-            });
     }
 
 
